9469e42a5a192e0968f521a3fdf4d9154a3ef80c
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-12-08 15:05:14 +0900

    add Ray.Di module

---
+++ tests/QueryLocatorModuleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Koriym\QueryLocator;
+
+use Koriym\QueryLocator\Exception\CountQueryException;
+use Koriym\QueryLocator\Exception\QueryFileNotFoundException;
+use Koriym\QueryLocator\Exception\ReadOnlyException;
+use Ray\Di\Injector;
+
+class QueryLocatorModuleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetSql()
+    {
+        /** @var $queryLocator QueryLocator */
+        $queryLocator = (new Injector(new QueryLocatorModule(__DIR__ . '/sql')))->getInstance(QueryLocatorInterface::class);
+        $this->assertInstanceOf(QueryLocatorInterface::class, $queryLocator);
+        $sql = $queryLocator->get('admin/user');
+        $expected = 'SELECT * FROM usr;';
+        $this->assertSame($expected, $sql);
+    }
+}
